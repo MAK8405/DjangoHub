7c56119560a26ee085ca147e293d216d513415f9
MAK8405 <mahdiahmadi2005kf@gmail.com>
2025-04-29 10:30:18 +0330

    Finish Admin and start Django Variables

--- members/views.py
+++ members/views.py
@@ -29,6 +29,6 @@ def main(request):
 def testing(request):
     template = loader.get_template("template.html")
     context = {
-        "fruits": ["Apple", "Banana", "Cherry"],
+        'firstname': 'Linus',
     }
     return HttpResponse(template.render(context, request))
