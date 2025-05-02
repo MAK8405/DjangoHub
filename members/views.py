515b004dd1654406e1728e75c56e31f91c4996cc
MAK8405 <mahdiahmadi2005kf@gmail.com>
2025-05-02 17:58:27 +0330

    Finish Djagno Tags and start Djano If Else

--- members/views.py
+++ members/views.py
@@ -27,9 +27,8 @@ def main(request):
 
 
 def testing(request):
-    mymembers = Member.objects.all().values()
     template = loader.get_template("template.html")
     context = {
-        "mymembers": mymembers,
+        "greeting": 3,
     }
     return HttpResponse(template.render(context, request))
